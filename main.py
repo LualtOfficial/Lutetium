34bae2b541ee1b47775d68a37654a4cb43dcd2ed
Lualt <officieel.lualt@gmail.com>
2022-07-06 17:12:26 +0200

    finished rewriting fixed all issues

--- main.py
+++ main.py
@@ -1,177 +1,128 @@
-from configparser import ConfigParser
 from os import system, listdir
-from datetime import datetime
+from threading import Thread
+from zmq import Context, REP
 import numpy as np
 import cv2 as cv
-import icons
 
-# Globabl Variables
-cursor = [0, -12, -12]
-old_cursor = [-12, -12]
-menu = [-121, False]
-trail = [[], True]
-shutdown = False
-config = ConfigParser()
-backgrounds = [0, [], False, False]
+from system import config
+from system import graphics
+
+# Public Variables
+CURSOR = [-12, -12, 0]
+FRAME = np.zeros((800, 480, 3), np.uint8)
+
+# Global Variables
+settings = {}
+cursor = CURSOR.copy()
+trail = []
+menu = [False, -120]
+hover = [False, False, False]
+backgrounds = [0, ["bg_space.png"]]
 
 
-def mouse_callback(event, x, y, *args):
-    global cursor, trail, menu, shutdown, old_cursor, backgrounds, background_img
-    
-    #print(cursor)
-    
-    # Cursor
-    cursor = [event, y, 480-x]
-    trail[0].append([y, 480-x])
-    if len(trail[0]) > 30:
-        del trail[0][0:len(trail) - 30]
-    with open("/home/lualt/Lutetium/cursor", "w+") as file:
-        if not menu[1]:
-            file.write(f"{event}\n{x}\n{y}")
-            old_cursor = [y, 480-x]
+# Functions
+def tcp_server():
+    global CURSOR, FRAME
+
+    ctx = Context()
+    sock = ctx.socket(REP)
+    sock.bind("tcp://*:32123")
+
+    while True:
+        msg = sock.recv()
+        if msg == b"get_cursor":
+            sock.send(" ".join([str(num) for num in CURSOR]).encode())
         else:
-            file.write(f"0\n{old_cursor[0]}\n{old_cursor[1]}")
-    
-    # Menu Buttons Long Press
-    if event == 2 and menu[0] == -1:
-        # Shutdown
-        if cursor[1] >= 37 and cursor[1] <= 82 and cursor[2] >= 381 and cursor[2] <= 421:
-            test = system("sudo shutdown -h 0")
-    # Menu Buttons Press
+            sock.send(b"")
+
+
+def cursor_callback(event, x, y, *_):
+    global cursor, trail, settings, CURSOR, hover, FRAME
+
+    cursor = [y, 480-x, event]
+
+    if not menu[0]:
+        CURSOR = cursor
+    if settings["trail"]:
+        trail.append([cursor[0], cursor[1]])
+        del trail[0:len(trail) - 30]
+
+    if event == 2:
+        menu[0] = not menu[0]
+        if menu[0] == 0 and 37 <= cursor[0] <= 82 and 381 <= cursor[1] <= 421:
+            system("sudo shutdown -h 0")
+
+    hover = [False]*len(hover)
+    if 37 <= cursor[0] <= 82 and 381 <= cursor[1] <= 421:
+        hover[0] = True
+    elif 0 <= cursor[0] <= 20 and 200 <= cursor[1] <= 220:
+        hover[1] = True
+    elif 100 <= cursor[0] <= 120 and 200 <= cursor[1] <= 220:
+        hover[2] = True
+
     if event == 4:
-        # Trail Toggle
-        if cursor[1] >= 25 and cursor[1] <= 96 and cursor[2] >= 126 and cursor[2] <= 153:
-            trail[1] = not trail[1]
-            config["DEFAULT"]["trail"] = "true" if trail[1] else "false"
-            with open("/home/lualt/Lutetium/settings.ini", "w+") as file:
-                config.write(file)
-        if cursor[1] >= 37 and cursor[1] <= 82 and cursor[2] >= 381 and cursor[2] <= 421:
+        if 25 <= cursor[0] <= 96 and 126 <= cursor[1] <= 153:
+            settings["trail"] = "false" if settings["trail"] == "true" else "true"
+            config.write(settings)
+        elif 37 <= cursor[0] <= 82 and 381 <= cursor[1] <= 421:
             exit()
-        # Background Picker
-        if cursor[1] >= 0 and cursor[1] <= 20 and cursor[2] >= 200 and cursor[2] <= 220:
+        elif 0 <= cursor[0] <= 20 and 200 <= cursor[1] <= 220:
             if backgrounds[0] == 0:
                 backgrounds[0] = len(backgrounds[1]) - 1
-                background_img = cv.imread(f"/home/lualt/Lutetium/images/{backgrounds[1][backgrounds[0]]}", -1)
-                config["DEFAULT"]["background"] = str(backgrounds[0])
-                with open("/home/lualt/Lutetium/settings.ini", "w+") as file:
-                    config.write(file)
             else:
                 backgrounds[0] -= 1
-                background_img = cv.imread(f"/home/lualt/Lutetium/images/{backgrounds[1][backgrounds[0]]}", -1)
-                config["DEFAULT"]["background"] = str(backgrounds[0])
-                with open("/home/lualt/Lutetium/settings.ini", "w+") as file:
-                    config.write(file)
-        elif cursor[1] >= 100 and cursor[1] <= 120 and cursor[2] >= 200 and cursor[2] <= 220:
+            settings["background"] = backgrounds[1][backgrounds[0]]
+            config.write(settings)
+            FRAME = cv.GaussianBlur(cv.imread("/home/lualt/Lutetium/images/" + settings["background"]), (31, 31), 0)
+        elif 100 <= cursor[0] <= 120 and 200 <= cursor[1] <= 220:
             if backgrounds[0] == len(backgrounds[1]) - 1:
                 backgrounds[0] = 0
-                background_img = cv.imread(f"/home/lualt/Lutetium/images/{backgrounds[1][backgrounds[0]]}", -1)
-                config["DEFAULT"]["background"] = str(backgrounds[0])
-                with open("/home/lualt/Lutetium/settings.ini", "w+") as file:
-                    config.write(file)
             else:
                 backgrounds[0] += 1
-                background_img = cv.imread(f"/home/lualt/Lutetium/images/{backgrounds[1][backgrounds[0]]}", -1)
-                config["DEFAULT"]["background"] = str(backgrounds[0])
-                with open("/home/lualt/Lutetium/settings.ini", "w+") as file:
-                    config.write(file)   
-    # Menu Buttons Hover
-        # Restart
-    if cursor[1] >= 37 and cursor[1] <= 82 and cursor[2] >= 381 and cursor[2] <= 421:
-        shutdown = True
-    else:
-        shutdown = False
-    if cursor[1] >= 0 and cursor[1] <= 20 and cursor[2] >= 200 and cursor[2] <= 220:
-        backgrounds[2] = True
-    elif cursor[1] >= 100 and cursor[1] <= 120 and cursor[2] >= 200 and cursor[2] <= 220:
-        backgrounds[3] = True
-    else:
-        backgrounds[2] = False
-        backgrounds[3] = False
-    
-    # Show/Hide Menu
-    if event == 2:
-        menu[1] = not menu[1]
+            settings["background"] = backgrounds[1][backgrounds[0]]
+            config.write(settings)
+            FRAME = cv.GaussianBlur(cv.imread("/home/lualt/Lutetium/images/" + settings["background"]), (31, 31), 0)
 
 
 if __name__ == "__main__":
     cv.namedWindow("Lutetium", cv.WND_PROP_FULLSCREEN)
     cv.setWindowProperty("Lutetium", cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
-    cv.setMouseCallback("Lutetium", mouse_callback)
-    
-    config.read("/home/lualt/Lutetium/settings.ini")
-    trail[1] = True if config["DEFAULT"]["trail"] == "true" else False
-    backgrounds[0] = int(config["DEFAULT"]["background"])
+    cv.setMouseCallback("Lutetium", cursor_callback)
 
-    background_imgs = listdir("/home/lualt/Lutetium/images")
-    for bg_img in background_imgs:
-        if bg_img[:3] == "bg_" and bg_img[-4:] == ".png":
+    tcp_thread = Thread(target=tcp_server, args=())
+    tcp_thread.daemon = True
+    tcp_thread.start()
+
+    settings = config.read()
+    for bg_img in listdir("/home/lualt/Lutetium/images/"):
+        if bg_img[:3] == "bg_" and bg_img != "bg_space.png":
             backgrounds[1].append(bg_img)
+    backgrounds[0] = backgrounds[1].index(settings["background"])
+    FRAME = cv.imread("/home/lualt/Lutetium/images/" + settings["background"])
 
-    print(f"/home/lualt/Lutetium/{backgrounds[1][backgrounds[0]]}")
-    background_img = cv.imread(f"/home/lualt/Lutetium/images/{backgrounds[1][backgrounds[0]]}", -1)
-    blurred_frame = background_img.copy()
-    
     while True:
-        img = background_img.copy()
-        
-        #keyboards.numbers(img, (0, 320), (0, 0, 0), (245, 66, 90), (203, 245, 66))
+        img = FRAME.copy()
 
-        # Menu
-            # Blur
-        if menu[1] and menu[0] == -121:
-            blurred_frame = cv.GaussianBlur(img, (31, 31), 0)
-        if menu[1]:
-            img = blurred_frame.copy()
-            # Offset
-        if menu[1] and menu[0] < -10:
-            menu[0] += 10
-        elif not menu[1] and menu[0] > -121:
-            menu[0] -= 10
-            # Shape
-        points = np.array([[0+menu[0], 0], [120+menu[0], 40], [120+menu[0], 440], [0+menu[0], 480]])
-        if menu[1]: cv.polylines(img, [points], True, (0, 0, 0), 3, 16)
-        cv.fillPoly(img, [points], (223, 232, 60), lineType=16)
-            # Time and Date
-        now = datetime.now()
-        time = now.strftime("%H:%M:%S")
-        date = now.strftime("%d-%m-%y")
-        cv.putText(img, time, (3+menu[0], 80), 2, 0.8, (0, 0, 0), 1, 16)
-        cv.putText(img, date, (9+menu[0], 100), 2, 0.6, (0, 0, 0), 1, 16)
-            # Trail toggle
-        if trail[1]:
-            img = icons.slider_right(img, (25+menu[0], 125), (255, 255, 255), (0, 200, 0))
-        else:
-            img = icons.slider_left(img, (25+menu[0], 125), (255, 255, 255), (0, 0, 200))
-        cv.putText(img, "Cursor Trail", (2+menu[0], 175), 2, 0.6, (0, 0, 0), 1, 16)
-            # Background Picker
-        points = np.array([[23+menu[0], 200], [23+menu[0], 220], [5+menu[0], 210]])
-        if backgrounds[2]:
-            cv.fillPoly(img, [points], (150, 150, 150), lineType=16)
-        else:
-            cv.fillPoly(img, [points], (0, 0, 0), lineType=16)
-        points = np.array([[97+menu[0], 200], [97+menu[0], 220], [115+menu[0], 210]])
-        if backgrounds[3]:
-            cv.fillPoly(img, [points], (150, 150, 150), lineType=16)
-        else:
-            cv.fillPoly(img, [points], (0, 0, 0), lineType=16)
-        text = cv.getTextSize(backgrounds[1][backgrounds[0]][3:-4], 2, 0.7, 1)[0]
-        cv.putText(img, backgrounds[1][backgrounds[0]][3:-4], (int(60 - text[0]/2)+menu[0], int(223 - text[1]/2)), 2, 0.7, (0, 0, 0), 1, 16)
-            # Shutdown and Restart
-        if shutdown:
-            img = icons.shutdown(img, (50+menu[0], 378), (150, 150, 150), (223, 232, 60))
-        else:
-            img = icons.shutdown(img, (50+menu[0], 378), (0, 0, 0), (223, 232, 60))
-        
-        # Trail
-        if trail[1]:
-            points = np.array(trail[0], np.int32)
+        if menu[0] and menu[1] == -120:
+            FRAME = cv.GaussianBlur(FRAME, (31, 31), 0)
+        elif not menu[0] and menu[1] == 0:
+            FRAME = cv.imread("/home/lualt/Lutetium/images/" + settings["background"])
+        if menu[0] and menu[1] != 0:
+            menu[1] += 10
+        elif not menu[0] and menu[1] != -120:
+            menu[1] -= 10
+
+        if menu[1] != -120:
+            img = graphics.global_menu(img, (menu[1], 0), (0, 0, 0), (0, 0, 0), (233, 232, 60), (150, 150, 150), settings, hover)
+
+        if settings["trail"] == "true":
+            points = np.array(trail, np.int32)
             cv.polylines(img, [points], False, (0, 0, 0), 3, 16)
             cv.polylines(img, [points], False, (255, 255, 255), 1, 16)
-    
-        # Cursor
-        cv.circle(img, (cursor[1], cursor[2]), 12, (0, 0, 0), -1, 16)
-        cv.circle(img, (cursor[1], cursor[2]), 10, (255, 255, 255), -1, 16)
-        
+
+        cv.circle(img, (cursor[0], cursor[1]), 12, (0, 0, 0), -1, 16)
+        cv.circle(img, (cursor[0], cursor[1]), 10, (255, 255, 255), -1, 16)
+
         img = cv.rotate(img, 0)
         cv.imshow("Lutetium", img)
         cv.waitKey(1)
--- system/graphics.py
+++ system/graphics.py
@@ -48,16 +48,7 @@ def slider_right(img, cords, color, bg):
     return img
 
 
-def global_menu(img, cords, color, border, bg, hover, settings):
-    ctx = Context()
-    sock = ctx.socket(REQ)
-    sock.connect("tcp://localhost:32123")
-    sock.send(b"get_cursor")
-    msg = sock.recv()
-    cursor = []
-    for num in msg.split(b" "):
-        cursor.append(int(num.decode()))
-
+def global_menu(img, cords, color, border, bg, hover, settings, hovers):
     points = np.array([[0+cords[0], 0+cords[1]], [120+cords[0], 40+cords[1]], [120+cords[0], 440+cords[1]], [0+cords[0], 480+cords[1]]])
     cv.polylines(img, [points], True, border, 3, 16)
     cv.fillPoly(img, [points], bg, lineType=16)
@@ -66,19 +57,19 @@ def global_menu(img, cords, color, border, bg, hover, settings):
     date = now.strftime("%d-%m-%y")
     cv.putText(img, time, (3+cords[0], 80+cords[1]), 2, 0.8, color, 1, 16)
     cv.putText(img, date, (9+cords[0], 100+cords[1]), 2, 0.6, color, 1, 16)
-    if settings["trail"]:
+    if settings["trail"] == "true":
         img = slider_right(img, (25+cords[0], 125+cords[1]), (255, 255, 255), (0, 200, 0))
     else:
         img = slider_left(img, (25+cords[0], 125+cords[1]), (255, 255, 255), (0, 0, 200))
     cv.putText(img, "Cursor Trail", (2+cords[0], 175+cords[1]), 2, 0.6, (0, 0, 0), 1, 16)
 
     points = np.array([[23+cords[0], 200+cords[1]], [23+cords[0], 220+cords[1]], [5+cords[0], 210+cords[1]]])
-    if 0+cords[0] <= cursor[0] <= 20+cords[0] and 200+cords[1] <= cursor[1] <= 220+cords[1]:
+    if hovers[1]:
         cv.fillPoly(img, [points], hover, lineType=16)
     else:
         cv.fillPoly(img, [points], color, lineType=16)
     points = np.array([[97+cords[0], 200+cords[1]], [97+cords[0], 220+cords[1]], [115+cords[0], 210+cords[1]]])
-    if 100+cords[0] <= cursor[0] <= 120+cords[0] and 200+cords[1] <= cursor[1] <= 220+cords[1]:
+    if hovers[2]:
         cv.fillPoly(img, [points], hover, lineType=16)
     else:
         cv.fillPoly(img, [points], color, lineType=16)
@@ -86,7 +77,7 @@ def global_menu(img, cords, color, border, bg, hover, settings):
     text = cv.getTextSize(settings["background"][3:-4], 2, 0.7, 1)[0]
     cv.putText(img, settings["background"][3:-4], (int(60 - text[0] / 2)+cords[0], int(223 - text[1] / 2)+cords[1]), 2, 0.7, (0, 0, 0), 1, 16)
 
-    if 37+cords[0] <= cursor[1] <= 82+cords[0] and 381+cords[1] <= cursor[2] <= 421+cords[1]:
+    if hovers[0]:
         img = shutdown(img, (50+cords[0], 378+cords[1]), hover, bg)
     else:
         img = shutdown(img, (50+cords[0], 378+cords[1]), color, bg)
